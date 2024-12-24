ea7d14dbd2ebb9ae14d36c115e06cd9384e0aa15
JeongEonJun <125655249+frozzun@users.noreply.github.com>
2024-12-24 19:07:28 +0900

    [Bronze II] Title: 진법 변환, Time: 108 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    int Num=0;
+
+    String[] parts = bufferedReader.readLine().split(" ");
+    String N = parts[0];
+    String bb = parts[1];
+
+    int B = Integer.parseInt(bb);
+
+    char[] c = N.toCharArray();
+
+    int length = N.length();
+
+    for(int i = 0; i < length; i++) {
+      if(c[i]>=65 && c[i]<=90) {
+        double d = Math.pow(B, length-i-1);
+        int dd = (int)d;
+        Num = Num + (c[i]-55)*dd;
+
+      }
+
+      else {
+        double d = Math.pow(B, length-i-1);
+        int dd = (int)d;
+        Num = Num + (c[i]-48)*dd;
+      }
+    }
+    System.out.println(Num);
+  }
+}
+
+
+
+
