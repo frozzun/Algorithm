f7ec93f528de3134515e2c80068ab9d9ef939389
JeongEonJun <125655249+frozzun@users.noreply.github.com>
2025-01-06 16:31:43 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1676 ms, Memory: 300876 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -6,16 +6,27 @@ public class Main {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    /*
+     * 카운팅 정렬 사용
+     * 수의 범위 ( 0 ~ 10000) 사실상 0은 제외
+     */
+    int[] cnt = new int[10001];
+
     int N = Integer.parseInt(br.readLine());
-    int[] indexes = new int[N];
+
     for (int i = 0; i < N; i++) {
-      indexes[i] = Integer.parseInt(br.readLine());
+      // 해당 인덱스의 값을 1 증가
+      cnt[Integer.parseInt(br.readLine())]++;
     }
 
-    Arrays.sort(indexes);
-    for (int i = 0; i < N; i++) {
-      bw.write(indexes[i] + "\n");
+    for (int i = 1; i < 10001; i++) {
+      // i 값이 개수가 0 이 될 때 까지 출력 (빈도수를 의미)
+      while (cnt[i] > 0) {
+        bw.write(i + "\n");
+        cnt[i]--;
+      }
     }
     bw.flush();
+    bw.close();
   }
 }
