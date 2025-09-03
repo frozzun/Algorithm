0e5d7d20623aa87204398800a4561190f7748c09
정언준 <125655249+frozzun@users.noreply.github.com>
2025-09-03 14:25:57 +0900

    [level 1] Title: 모의고사, Time: 5.24 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    // answer[] : 정답 배열
+    public int[] solution(int[] answers) {
+        
+        int[][] patterns = {
+        {1, 2, 3, 4, 5}, 
+        {2, 1, 2, 3, 2, 4, 2, 5}, 
+        {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
+        int[] scores = new int[3];
+        
+        for(int i=0;i<answers.length;i++) {
+            for(int j=0;j<patterns.length;j++) {
+                if(answers[i]==patterns[j][i%patterns[j].length]) scores[j] ++;
+            }
+        }
+        int maxScore = Arrays.stream(scores).max().getAsInt();
+        
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        for(int i=0;i<scores.length;i++) {
+            if(scores[i]==maxScore) {
+                answer.add(i+1);
+            }
+        }
+        
+        // 가장 많은 문제를 맞힌 사람 return
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
